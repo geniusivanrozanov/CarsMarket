5b11859c037a54b3af676a2ebb58671fd0ca421b
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-23 22:50:37 +0300

    add entity interfaces

---
+++ Services/IdentityService/IdentityService.Domain/Interfaces/IAuditable.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityService.Domain.Interfaces;
+
+public interface IAuditable
+{
+    public DateTimeOffset CreatedAt { get; set; }
+    
+    public DateTimeOffset LastModifiedAt { get; set; }
+}
