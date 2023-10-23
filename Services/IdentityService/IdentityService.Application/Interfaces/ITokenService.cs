10468e1ff6d60af8aa2cc2c2ebc00256041d057b
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-24 01:10:59 +0300

    feat: add token and user services interfaces

---
+++ Services/IdentityService/IdentityService.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using IdentityService.Domain.Entities;
+
+namespace IdentityService.Application.Interfaces;
+
+public interface ITokenService
+{
+    Task<string> GenerateAccessTokenAsync(User user);
+}
---
+++ Services/IdentityService/IdentityService.Application/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using IdentityService.Application.DTOs;
+using IdentityService.Application.QueryParameters;
+
+namespace IdentityService.Application.Interfaces;
+
+public interface IUserService
+{
+    Task<UserDto> RegisterUserAsync(RegisterDto register);
+    Task<LoginResultDto> LoginUserAsync(LoginDto login);
+    Task<IEnumerable<UserDto>> GetUsersAsync(UserQueryParameters userQueryParameters);
+    Task<UserDto> GetUserByIdAsync(Guid userId);
+    Task<UserDto> SetUserStatusByIdAsync(Guid userId, string status);
+}
