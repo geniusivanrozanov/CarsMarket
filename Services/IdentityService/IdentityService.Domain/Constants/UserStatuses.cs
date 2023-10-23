3c305cdb35287633e6fd72ce375d53eec0d293a1
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-23 22:51:57 +0300

    add domain user entity

---
+++ Services/IdentityService/IdentityService.Domain/Constants/UserStatuses.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityService.Domain.Constants;
+
+public static class UserStatuses
+{
+    public const string Active = nameof(Active);
+    public const string Locked = nameof(Locked);
+}
---
+++ Services/IdentityService/IdentityService.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using IdentityService.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Domain.Entities;
+
+public class User : IdentityUser<Guid>, IEntity, IAuditable
+{
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset LastModifiedAt { get; set; }
+    
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+
+    public required string Status { get; set; }
+}
