2885758e55d2993801899ae4eaae680637ec156e
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-24 01:56:35 +0300

    feat: add user repository interface

---
+++ Services/IdentityService/IdentityService.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using IdentityService.Application.QueryParameters;
+using IdentityService.Domain.Entities;
+
+namespace IdentityService.Application.Interfaces;
+
+public interface IUserRepository
+{
+    Task<TProjection> GetUserByIdAsync<TProjection>(Guid userId, Func<UserEntity, TProjection> map);
+    Task<IEnumerable<TProjection>> GetUsersAsync<TProjection>(UserQueryParameters userQueryParameters, Func<UserEntity, TProjection> map);
+}
