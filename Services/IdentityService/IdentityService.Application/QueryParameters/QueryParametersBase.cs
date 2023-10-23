d8a53b3fa90a147add5a5da1d2c8a90cce8bb69f
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-23 23:52:54 +0300

    feat: add query parameters

---
+++ Services/IdentityService/IdentityService.Application/QueryParameters/QueryParametersBase.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityService.Application.QueryParameters;
+
+public abstract record QueryParametersBase
+{
+    public int? Page { get; set; }
+    public int? PageSize { get; set; }
+    public string? OrderBy { get; set; }
+    public string? OrderDirection { get; set; }
+}
---
+++ Services/IdentityService/IdentityService.Application/QueryParameters/UserQueryParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityService.Application.QueryParameters;
+
+public record UserQueryParameters : QueryParametersBase
+{
+    public string? Status { get; init; }
+    public string? Email { get; init; }
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+}
