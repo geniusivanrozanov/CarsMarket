4ccf6dc944872e777f0496afc1ea6bad037e7596
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-23 23:16:44 +0300

    add application DTOs

---
+++ Services/IdentityService/IdentityService.Application/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityService.Application.DTOs;
+
+public record UserDto
+{
+    public required string Email { get; init; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+}
