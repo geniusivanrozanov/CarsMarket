bea488e849b2257285c9595a09def059d454e411
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-24 01:21:41 +0300

    feat: add mapper interface

---
+++ Services/IdentityService/IdentityService.Application/Interfaces/IMapper.cs
@@ -0,0 +1,10 @@
+﻿using IdentityService.Application.DTOs;
+using IdentityService.Domain.Entities;
+
+namespace IdentityService.Application.Interfaces;
+
+public interface IMapper
+{
+    UserEntity ToUserEntity(RegisterDto registerDto);
+    UserDto ToUserDto(UserEntity userEntity);
+}
