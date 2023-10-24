e12a431fc129c986feabdf57a28f936e6149c8e0
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-24 12:52:09 +0300

    feat: add token service implementation

---
+++ Services/IdentityService/IdentityService.Application/Options/TokenOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityService.Application.Options;
+
+public record TokenOptions
+{
+    public required string Key { get; init; }
+    public required int ExpirationMinutes { get; init; }
+}
---
+++ Services/IdentityService/IdentityService.Application/Services/TokenService.cs
@@ -0,0 +1,43 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using IdentityService.Application.Interfaces;
+using IdentityService.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using TokenOptions = IdentityService.Application.Options.TokenOptions;
+
+namespace IdentityService.Application.Services;
+
+public class TokenService(
+    IOptions<TokenOptions> tokenOptions,
+    UserManager<UserEntity> userManager)
+    : ITokenService
+{
+    public async Task<string> GenerateAccessTokenAsync(UserEntity userEntity)
+    {
+        var key = Encoding.UTF8.GetBytes(tokenOptions.Value.Key);
+        var securityKey = new SymmetricSecurityKey(key);
+
+        var rolesList = await userManager.GetRolesAsync(userEntity);
+        var roles = string.Join(", ", rolesList);
+        
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new []
+            {
+                new Claim(ClaimTypes.NameIdentifier, userEntity.Id.ToString()),
+                new Claim(ClaimTypes.Role, roles),
+                new Claim(ClaimTypes.Email, userEntity.Email!)
+            }),
+            SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature),
+            Expires = DateTime.Now.AddMinutes(tokenOptions.Value.ExpirationMinutes)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
