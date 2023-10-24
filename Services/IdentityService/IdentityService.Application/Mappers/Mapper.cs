87457532cd21446c7681be5f506d0169406f8533
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-24 13:09:42 +0300

    feat: add mapper implementation

---
+++ Services/IdentityService/IdentityService.Application/Mappers/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using IdentityService.Application.DTOs;
+using IdentityService.Application.Interfaces;
+using IdentityService.Domain.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace IdentityService.Application.Mappers;
+
+[Mapper]
+public partial class Mapper : IMapper
+{   
+    public partial UserEntity ToUserEntity(RegisterDto registerDto);
+
+    public partial UserDto ToUserDto(UserEntity userEntity);
+}
