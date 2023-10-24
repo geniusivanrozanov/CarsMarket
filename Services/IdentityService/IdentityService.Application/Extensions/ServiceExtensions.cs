c1aa3981f7b96ec0059db6ba20ac65f2ace3b6c7
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-24 16:59:34 +0300

    feat: add DI container extensions

---
+++ Services/IdentityService/IdentityService.Application/Extensions/ServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using IdentityService.Application.Interfaces;
+using IdentityService.Application.Mappers;
+using IdentityService.Application.Options;
+using IdentityService.Application.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityService.Application.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddServices()
+            .AddMappers()
+            .ConfigureOptions(configuration);
+        
+        return services;
+    }
+    
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserService, UserService>();
+        
+        return services;
+    }
+
+    private static IServiceCollection AddMappers(this IServiceCollection services)
+    {
+        services.AddScoped<IMapper, Mapper>();
+        
+        return services;
+    }
+    
+    private static IServiceCollection ConfigureOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+        
+        return services;
+    }
+}
