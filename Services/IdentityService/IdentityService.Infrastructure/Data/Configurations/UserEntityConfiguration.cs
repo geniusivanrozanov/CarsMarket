48c82c850833d7653d96626e50d7684e009ecaf4
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-25 21:43:13 +0300

    feat: add identity context with user configuration

---
+++ Services/IdentityService/IdentityService.Infrastructure/Data/Configurations/UserEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IdentityService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityService.Infrastructure.Data.Configurations;
+
+public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+    private const int FirstNameMaxLength = 64;
+    private const int LastNameMaxLength = 64;
+    
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.Property(u => u.FirstName)
+            .HasMaxLength(FirstNameMaxLength);
+
+        builder.Property(u => u.LastName)
+            .HasMaxLength(LastNameMaxLength);
+
+        builder.Property(u => u.Email)
+            .IsRequired();
+    }
+}
---
+++ Services/IdentityService/IdentityService.Infrastructure/Data/Contexts/IdentityContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using IdentityService.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Infrastructure.Data.Contexts;
+
+public class IdentityContext
+    (DbContextOptions options) : IdentityDbContext<UserEntity, IdentityRole<Guid>, Guid>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        base.OnModelCreating(builder);
+    }
+}
