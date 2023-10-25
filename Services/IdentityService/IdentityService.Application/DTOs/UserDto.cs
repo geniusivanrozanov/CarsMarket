81bad365f9ff8720f89ad1d075eda34ddfe2e44a
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-26 02:46:07 +0300

    feat: add id field to user dto

--- Services/IdentityService/IdentityService.Application/DTOs/UserDto.cs
+++ Services/IdentityService/IdentityService.Application/DTOs/UserDto.cs
@@ -2,6 +2,7 @@
 
 public record UserDto
 {
+    public required Guid Id { get; init; }
     public required string Email { get; init; }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
