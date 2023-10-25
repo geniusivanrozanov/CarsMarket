a474e139f1b91be70640704af4c569c6dca7660c
Ivan Rozanov <navi.zor9@gmail.com>
2023-10-26 00:07:47 +0300

    feat: add infrastructure DI container extensions

---
+++ Services/IdentityService/IdentityService.Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,60 @@
+﻿using IdentityService.Application.Interfaces;
+using IdentityService.Infrastructure.Data.Contexts;
+using IdentityService.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityService.Infrastructure.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddDbContexts(configuration)
+            .AddRepositories()
+            .MigrateDatabase<IdentityContext>();
+    }
+
+    private static IServiceCollection AddDbContexts(this IServiceCollection services, IConfiguration configuration)
+    {
+        var identityConnectionString = configuration.GetConnectionString("IdentityPostgres");
+        ArgumentException.ThrowIfNullOrEmpty(identityConnectionString);
+        
+        services.AddDbContext<IdentityContext>(options =>
+        {
+            options.UseNpgsql(identityConnectionString);
+        });
+        
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        
+        return services;
+    }
+
+    private static IServiceCollection MigrateDatabase<TContext>(this IServiceCollection services)
+        where TContext : DbContext
+    {
+        var serviceProvider = services.BuildServiceProvider();
+        var context = serviceProvider.GetRequiredService<TContext>();
+
+        try
+        {
+            context.Database.Migrate();
+        }
+        catch (Exception)
+        {
+            var logger = serviceProvider.GetRequiredService<ILogger<TContext>>();
+            logger.LogError("Failed to apply {Context} migrations", typeof(TContext).Name);
+            throw;
+        }
+        
+        return services;
+    }
+}
