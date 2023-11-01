d86cdb25581666e1df8a498b65ff1d22f81b0ce7
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-01 17:05:08 +0300

    feat: add refresh token repository interface

---
+++ Services/IdentityService/IdentityService.Application/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityService.Application.Interfaces;
+
+public interface IRefreshTokenRepository
+{
+    Task SetRefreshTokenAsync(string token, Guid userId);
+    Task<Guid?> GetUserIdByTokenAsync(string token);
+    Task DeleteTokenAsync(string token);
+}
--- Services/IdentityService/IdentityService.Application/Interfaces/ITokenService.cs
+++ Services/IdentityService/IdentityService.Application/Interfaces/ITokenService.cs
@@ -5,4 +5,6 @@ namespace IdentityService.Application.Interfaces;
 public interface ITokenService
 {
     Task<string> GenerateAccessTokenAsync(UserEntity userEntity);
+    Task<string> GenerateAndSaveRefreshTokenAsync(Guid userId);
+    Task<Guid> GetUserIdByRefreshToken(string token);
 }
