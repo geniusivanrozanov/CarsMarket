3c532de4f6deb7bb2817468984af735abcbd428b
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-02 23:32:05 +0300

    feat: add serilogger to identity web api

--- Services/IdentityService/IdentityService.WebAPI/Program.cs
+++ Services/IdentityService/IdentityService.WebAPI/Program.cs
@@ -1,15 +1,20 @@
+using Common.Logging;
 using IdentityService.Application.Extensions;
 using IdentityService.Infrastructure.Extensions;
 using IdentityService.WebAPI.Extensions;
 using IdentityService.WebAPI.Middlewares;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
+
+builder.Host.UseSerilog(SeriLogger.Configure);
 
 builder.Services
     .AddInfrastructureLayer(configuration)
     .AddApplicationLayer(configuration)
     .AddApiLayer();
+
 
 var app = builder.Build();
 
