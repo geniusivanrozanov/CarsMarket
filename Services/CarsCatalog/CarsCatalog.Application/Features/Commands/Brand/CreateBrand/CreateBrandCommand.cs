b59c607f7f93c5760525417e5aeb4476fe1bd688
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-09 23:30:46 +0300

    feat: add create brand command

---
+++ Services/CarsCatalog/CarsCatalog.Application/Features/Commands/Brand/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,9 @@
+﻿using CarsCatalog.Application.DTOs;
+using MediatR;
+
+namespace CarsCatalog.Application.Features.Commands;
+
+public class CreateBrandCommand(CreateBrandDto createBrandDto) : IRequest<GetBrandDto>
+{
+    public CreateBrandDto CreateBrandDto { get; } = createBrandDto;
+}
---
+++ Services/CarsCatalog/CarsCatalog.Application/Features/Commands/Brand/CreateBrand/CreateBrandCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CarsCatalog.Application.DTOs;
+using CarsCatalog.Application.Interfaces.Mappers;
+using CarsCatalog.Application.Interfaces.Repositories;
+using CarsCatalog.Domain.Entities;
+using MediatR;
+
+namespace CarsCatalog.Application.Features.Commands;
+
+public class CreateBrandCommandHandler(
+    IRepositoryUnitOfWork repositoryUnitOfWork,
+    IMapper mapper) :
+    IRequestHandler<CreateBrandCommand, GetBrandDto>
+{
+    public async Task<GetBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    {
+        var entity = mapper.Map<BrandEntity, CreateBrandDto>(request.CreateBrandDto);
+        
+        repositoryUnitOfWork.Brands.CreateBrand(entity);
+        await repositoryUnitOfWork.SaveAsync(cancellationToken);
+
+        var dto = mapper.Map<GetBrandDto, BrandEntity>(entity);
+
+        return dto;
+    }
+}
