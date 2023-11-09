a7595694ca26cc6b3937fc6435018db3b6b4acc9
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-09 23:21:58 +0300

    feat: add mapper interface

---
+++ Services/CarsCatalog/CarsCatalog.Application/Interfaces/Mappers/IMapper.cs
@@ -0,0 +1,10 @@
+﻿using CarsCatalog.Application.DTOs;
+using CarsCatalog.Domain.Entities;
+
+namespace CarsCatalog.Application.Interfaces.Mappers;
+
+public interface IMapper
+{
+    TResult Map<TResult, TSource>(TSource source);
+    IQueryable<TResult> Project<TResult, TSource>(IQueryable<TSource> source);
+}
