fac749630232e8aed89f8a54953e15e859aa3f4a
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-09 23:03:37 +0300

    feat: add brand DTOs

---
+++ Services/CarsCatalog/CarsCatalog.Application/DTOs/Brand/GetBrandDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CarsCatalog.Application.DTOs;
+
+public class GetBrandDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; } 
+}
---
+++ Services/CarsCatalog/CarsCatalog.Application/DTOs/Brand/UpdateBrandDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CarsCatalog.Application.DTOs;
+
+public class UpdateBrandDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; } 
+}
