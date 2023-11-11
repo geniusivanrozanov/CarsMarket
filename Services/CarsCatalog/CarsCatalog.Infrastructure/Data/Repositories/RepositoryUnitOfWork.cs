241979c3467aeec8fa7ffdc5060136c715cb0e92
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-12 00:47:14 +0300

    feat: add repository unit of work implementation

---
+++ Services/CarsCatalog/CarsCatalog.Infrastructure/Data/Repositories/RepositoryUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using CarsCatalog.Application.Interfaces.Repositories;
+using CarsCatalog.Infrastructure.Data.Contexts;
+
+namespace CarsCatalog.Infrastructure.Data.Repositories;
+
+public class RepositoryUnitOfWork(CatalogContext context) : IRepositoryUnitOfWork
+{
+    private IBrandRepository? _brandRepository;
+
+    public IBrandRepository Brands => _brandRepository ??= new BrandRepository(context);
+    public async Task SaveAsync(CancellationToken cancellationToken)
+    {
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
