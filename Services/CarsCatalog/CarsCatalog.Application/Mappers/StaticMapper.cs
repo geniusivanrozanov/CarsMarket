44e1395c2a2f9f4a1a7b3a3d28e1b184d6c3622d
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-13 09:53:35 +0300

    feat: add infrastructure DI registration

--- Services/CarsCatalog/CarsCatalog.Application/Mappers/StaticMapper.cs
+++ Services/CarsCatalog/CarsCatalog.Application/Mappers/StaticMapper.cs
@@ -10,15 +10,17 @@ public static partial class StaticMapper
     public static partial GetBrandDto ToGetBrandDto(this BrandEntity brandEntity);
     public static partial BrandEntity ToBrandEntity(this CreateBrandDto createBrandDto);
     public static partial BrandEntity ToBrandEntity(this UpdateBrandDto updateBrandDto);
+    public static partial void ToBrandEntity(this UpdateBrandDto updateBrandDto, BrandEntity brandEntity);
+
     public static partial IQueryable<GetBrandDto> ProjectToGetBrandDto(this IQueryable<BrandEntity> queryable);
     
     public static partial TResult MapTo<TResult>(this object source);
 
     public static IQueryable<TResult> ProjectTo<TResult>(this IQueryable<object> queryable)
     {
-        if (typeof(TResult) == typeof(object))
+        if (queryable is IQueryable<TResult> query)
         {
-            return (IQueryable<TResult>)queryable;
+            return query;
         }
 
         return queryable.MapTo<IQueryable<TResult>>();
---
+++ Services/CarsCatalog/CarsCatalog.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using CarsCatalog.Application.Interfaces.Repositories;
+using CarsCatalog.Infrastructure.Data.Contexts;
+using CarsCatalog.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarsCatalog.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddDbContexts(configuration)
+            .AddRepositories();
+    }
+
+    private static IServiceCollection AddDbContexts(this IServiceCollection services, IConfiguration configuration)
+    {
+        var catalogConnectionString = configuration.GetConnectionString("CatalogPostgres");
+        ArgumentException.ThrowIfNullOrEmpty(catalogConnectionString);
+
+        services.AddDbContext<CatalogContext>((provider, builder) =>
+        {
+            builder.UseNpgsql(catalogConnectionString);
+        });
+        
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IRepositoryUnitOfWork, RepositoryUnitOfWork>();
+        
+        return services;
+    }
+}
