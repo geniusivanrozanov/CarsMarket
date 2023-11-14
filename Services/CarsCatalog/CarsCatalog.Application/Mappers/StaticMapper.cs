1b0eced220800808ea79cb3648e9d182df47b989
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-14 21:59:19 +0300

    feat: add model mappings

--- Services/CarsCatalog/CarsCatalog.Application/Mappers/StaticMapper.cs
+++ Services/CarsCatalog/CarsCatalog.Application/Mappers/StaticMapper.cs
@@ -11,8 +11,14 @@ public static partial class StaticMapper
     public static partial BrandEntity ToBrandEntity(this CreateBrandDto createBrandDto);
     public static partial BrandEntity ToBrandEntity(this UpdateBrandDto updateBrandDto);
     public static partial void ToBrandEntity(this UpdateBrandDto updateBrandDto, BrandEntity brandEntity);
-
     public static partial IQueryable<GetBrandDto> ProjectToGetBrandDto(this IQueryable<BrandEntity> queryable);
+    
+    public static partial GetModelDto ToGetModelDto(this ModelEntity modelEntity);
+    public static partial ModelEntity ToModelEntity(this CreateModelDto createModelDto);
+    public static partial ModelEntity ToModelEntity(this UpdateModelDto updateModelDto);
+    public static partial void ToModelEntity(this UpdateModelDto updateModelDto, ModelEntity modelEntity);
+    public static partial IQueryable<GetModelDto> ProjectToGetModelDto(this IQueryable<ModelEntity> queryable);
+
     
     public static partial TResult MapTo<TResult>(this object source);
 
