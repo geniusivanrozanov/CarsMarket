fcfd62eb16317e8b181c1a71342a097b2457f566
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-15 22:18:59 +0300

    feat: add model entity configuration

---
+++ Services/CarsCatalog/CarsCatalog.Infrastructure/Data/Configurations/ModelEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CarsCatalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarsCatalog.Infrastructure.Data.Configurations;
+
+public class ModelEntityConfiguration : IEntityTypeConfiguration<ModelEntity>
+{
+    private const int NameMaxLength = 64;
+    
+    public void Configure(EntityTypeBuilder<ModelEntity> builder)
+    {
+        builder.HasIndex(x => new { x.Name, x.BrandId })
+            .IsUnique();
+        
+        builder.Property(x => x.Name)
+            .HasMaxLength(NameMaxLength)
+            .IsRequired();
+    }
+}
