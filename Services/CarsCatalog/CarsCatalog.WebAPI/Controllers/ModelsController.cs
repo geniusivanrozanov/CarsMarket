8370a00aee99ff21e598a01a916152c8866dd6a7
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-15 22:52:18 +0300

    feat: add models controller

---
+++ Services/CarsCatalog/CarsCatalog.WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,52 @@
+﻿using CarsCatalog.Application.DTOs;
+using CarsCatalog.Application.Features.Commands;
+using CarsCatalog.Application.Features.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsCatalog.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ModelsController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetModels()
+    {
+        var dto = await mediator.Send(new GetModelsListQuery());
+
+        return Ok(dto);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetModelById(Guid id)
+    {
+        var dto = await mediator.Send(new GetModelByIdQuery(id));
+
+        return Ok(dto);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateModel([FromBody]CreateModelDto createModelDto)
+    {
+        var dto = await mediator.Send(new CreateModelCommand(createModelDto));
+
+        return CreatedAtAction(nameof(GetModelById), new {dto.Id}, dto);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateModel(Guid id, [FromBody]UpdateModelDto updateModelDto)
+    {
+        var dto = await mediator.Send(new UpdateModelCommand(id, updateModelDto));
+
+        return CreatedAtAction(nameof(GetModelById), new {dto.Id}, dto);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteModel(Guid id)
+    {
+        await mediator.Send(new DeleteModelCommand(id));
+
+        return NoContent();
+    }
+}
