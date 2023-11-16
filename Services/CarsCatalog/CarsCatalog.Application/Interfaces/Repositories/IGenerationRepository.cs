b73abd9542d1b1a13e145afed3a88d6d288a9b0b
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-16 23:22:30 +0300

    feat: add generation repository interface

---
+++ Services/CarsCatalog/CarsCatalog.Application/Interfaces/Repositories/IGenerationRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarsCatalog.Domain.Entities;
+
+namespace CarsCatalog.Application.Interfaces.Repositories;
+
+public interface IGenerationRepository
+{
+    Task<TProjection?> GetGenerationByIdAsync<TProjection>(Guid generationId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TProjection>> GetGenerationsAsync<TProjection>(CancellationToken cancellationToken = default);
+    Task<bool> ExistsWithIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<bool> ExistsWithNameAndModelIdAsync(string name, Guid modelId, CancellationToken cancellationToken = default);
+    void CreateGeneration(GenerationEntity generation);
+    void UpdateGeneration(GenerationEntity generation);
+    void DeleteGeneration(GenerationEntity generation);
+}
--- Services/CarsCatalog/CarsCatalog.Application/Interfaces/Repositories/IRepositoryUnitOfWork.cs
+++ Services/CarsCatalog/CarsCatalog.Application/Interfaces/Repositories/IRepositoryUnitOfWork.cs
@@ -4,6 +4,7 @@ public interface IRepositoryUnitOfWork
 {
     IBrandRepository Brands { get; }
     IModelRepository Models { get; }
+    IGenerationRepository Generations { get; }
 
     Task SaveAsync(CancellationToken cancellationToken);
 }
