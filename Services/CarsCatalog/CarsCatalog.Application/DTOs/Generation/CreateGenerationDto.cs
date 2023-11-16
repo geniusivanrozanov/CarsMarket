a8d72691db95e55f097efca514e660f88f57b448
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-16 23:19:45 +0300

    feat: add generation dtos

---
+++ Services/CarsCatalog/CarsCatalog.Application/DTOs/Generation/CreateGenerationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsCatalog.Application.DTOs;
+
+public class CreateGenerationDto
+{
+    public required string Name { get; set; }
+    public int StartYear { get; set; }
+    public int? EndYear { get; set; }
+    
+    public Guid ModelId { get; set; }
+}
