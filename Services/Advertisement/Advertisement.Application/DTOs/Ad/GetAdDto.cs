2c99abc00dc666806964e59dd25fc8b63a963162
Ivan Rozanov <navi.zor9@gmail.com>
2023-11-20 14:27:45 +0300

    feat: add dtos

---
+++ Services/Advertisement/Advertisement.Application/DTOs/Ad/GetAdDto.cs
@@ -0,0 +1,22 @@
+﻿using Advertisement.Domain.Enums;
+using MediatR;
+
+namespace Advertisement.Application.DTOs.Ad;
+
+public class GetAdDto : IRequest
+{
+    public Guid Id { get; set; }
+    public string? Description { get; set; }
+    public string? Vin { get; set; }
+    public string Brand { get; set; } = null!;
+    public string Model { get; set; } = null!;
+    public string Generation { get; set; } = null!;
+    public int Year { get; set; }
+    public int Mileage { get; set; }
+    public AdStatus Status { get; set; }
+    public double Price { get; set; }
+    public Currency Currency { get; set; }
+    
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+}
---
+++ Services/Advertisement/Advertisement.Application/DTOs/Ad/UpdateAdStatusDto.cs
@@ -0,0 +1,8 @@
+﻿using Advertisement.Domain.Enums;
+
+namespace Advertisement.Application.DTOs.Ad;
+
+public class UpdateAdStatusDto
+{
+    public AdStatus Status { get; set; }
+}
