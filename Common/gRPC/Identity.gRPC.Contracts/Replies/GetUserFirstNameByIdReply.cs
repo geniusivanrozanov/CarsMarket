7c265c452258d274a53cab82e79055bb81d69a84
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-06 17:10:01 +0300

    feat: add get first name by id reply

---
+++ Common/gRPC/Identity.gRPC.Contracts/Replies/GetUserFirstNameByIdReply.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+using Identity.gRPC.Contracts.Enums;
+
+namespace Identity.gRPC.Contracts.Replies;
+
+[DataContract]
+public class GetUserFirstNameByIdReply
+{
+    [DataMember(Order = 1)]
+    public string FirstName { get; set; } = null!;
+    
+    [DataMember(Order = 2)]
+    public Error? Error { get; set; }
+    
+    [DataMember(Order = 3)]
+    public string? ErrorMessage { get; set; }
+}
