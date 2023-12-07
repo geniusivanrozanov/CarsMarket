61bf5ed175c45f2d05df6b99cb89463343399192
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-07 14:41:28 +0300

    feat: add grpc identity service implementation

---
+++ Services/IdentityService/IdentityService.WebAPI/Services/IdentityService.cs
@@ -0,0 +1,38 @@
+﻿using Identity.gRPC.Contracts;
+using Identity.gRPC.Contracts.Enums;
+using Identity.gRPC.Contracts.Replies;
+using Identity.gRPC.Contracts.Requests;
+using IdentityService.Application.Exceptions;
+using IdentityService.Application.Interfaces;
+using ProtoBuf.Grpc;
+
+namespace IdentityService.WebAPI.Services;
+
+public class IdentityService : IIdentityService
+{
+    private readonly IUserService _userService;
+
+    public IdentityService(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public async Task<GetUserFirstNameByIdReply> GetUserFirstNameAsync(GetUserFirstNameByIdRequest request, CallContext context)
+    {
+        var reply = new GetUserFirstNameByIdReply();
+        
+        try
+        {
+            var user = await _userService.GetUserByIdAsync(request.UserId, context.CancellationToken);
+
+            reply.FirstName = user.FirstName;
+        }
+        catch (NotExistsException e)
+        {
+            reply.Error = Error.UserNotFound;
+            reply.ErrorMessage = e.Message;
+        }
+
+        return reply;
+    }
+}
