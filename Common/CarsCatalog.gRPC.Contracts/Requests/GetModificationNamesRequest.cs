ebbb6f5df4755c48f9f3d8b26fc74cec390e354e
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-12 12:45:55 +0300

    feat: add get modification names request

---
+++ Common/CarsCatalog.gRPC.Contracts/Requests/GetModificationNamesRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace CarsCatalog.gRPC.Contracts.Requests;
+
+[DataContract]
+public class GetModificationNamesRequest
+{
+    [DataMember(Order = 1)]
+    public Guid BrandId { get; set; }
+    [DataMember(Order = 2)]
+    public Guid ModelId { get; set; }
+    [DataMember(Order = 3)]
+    public Guid GenerationId { get; set; }
+}
