995087ce39cbdeb47b68981d725adf9515f6268a
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-13 13:18:01 +0300

    feat: add cars catalog grpc service interface

---
+++ Common/CarsCatalog.gRPC.Contracts/ICarsCatalogService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+using CarsCatalog.gRPC.Contracts.Replies;
+using CarsCatalog.gRPC.Contracts.Requests;
+using ProtoBuf.Grpc;
+
+namespace CarsCatalog.gRPC.Contracts;
+
+[ServiceContract]
+public interface ICarsCatalogService
+{
+    [OperationContract]
+    Task<GetModificationNamesReply> GetModificationNames(GetModificationNamesRequest request,
+        CallContext context = default);
+}
