4c6c07e333687432e272a51b8e2bf1af21fe4f46
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-21 14:18:29 +0300

    feat: add identity messages

---
+++ Common/Identity.Messages.Contracts/UserUpdatedMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Messages.Contracts;
+
+public class UserUpdatedMessage
+{
+    public Guid UserId { get; set; }
+    public string UpdatedFirstName { get; set; } = null!;
+    public string UpdatedLastName { get; set; } = null!;
+}
