4ad44b903c402c9b59232ae19e0e93a4f252b535
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-22 16:15:52 +0300

    feat: add mapping to user updated message

--- Services/IdentityService/IdentityService.Application/Mappers/Mapper.cs
+++ Services/IdentityService/IdentityService.Application/Mappers/Mapper.cs
@@ -1,4 +1,5 @@
-﻿using IdentityService.Application.DTOs;
+﻿using Identity.Messages.Contracts;
+using IdentityService.Application.DTOs;
 using IdentityService.Application.Interfaces;
 using IdentityService.Domain.Entities;
 using Riok.Mapperly.Abstractions;
@@ -14,6 +15,11 @@ public partial class Mapper : IMapper
     public partial UserEntity ToUserEntity(RegisterDto registerDto);
 
     public partial UserDto ToUserDto(UserEntity userEntity);
+    
+    [MapProperty(nameof(UserEntity.Id), nameof(UserUpdatedMessage.UserId))]
+    [MapProperty(nameof(UserEntity.FirstName), nameof(UserUpdatedMessage.UpdatedFirstName))]
+    [MapProperty(nameof(UserEntity.LastName), nameof(UserUpdatedMessage.UpdatedLastName))]
+    public partial UserUpdatedMessage ToUserUpdatedMessage(UserEntity userEntity);
 
     public partial void ToUserEntity(UpdateUserDto updateUserDto, UserEntity userEntity);
 }
