b162e7115fc998b3cd88bf34317f25bc7ccd0b8f
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-27 20:14:41 +0300

    feat: add domain entities

---
+++ Services/Chat/Chat.Domain/Entities/ChatEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Chat.Domain.Entities;
+
+public class ChatEntity
+{
+    public Guid Id { get; set; }
+    public Guid AdId { get; set; }
+    public ICollection<MemberEntity> Members { get; set; } = null!;
+}
