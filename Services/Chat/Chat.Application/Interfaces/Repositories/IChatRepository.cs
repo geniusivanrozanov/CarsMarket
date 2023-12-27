f49cc0dbad5bfd468a67aa941a699b088d51c18f
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-27 22:19:56 +0300

    feat: add repositories interfaces

---
+++ Services/Chat/Chat.Application/Interfaces/Repositories/IChatRepository.cs
@@ -0,0 +1,11 @@
+﻿using Chat.Domain.Entities;
+
+namespace Chat.Application.Interfaces.Repositories;
+
+public interface IChatRepository
+{
+    Task<IEnumerable<TProjection>> GetChatsByMemberIdAsync<TProjection>(Guid memberId,
+        CancellationToken cancellationToken = default);
+
+    Task CreateChatAsync(ChatEntity entity, CancellationToken cancellationToken = default);
+}
