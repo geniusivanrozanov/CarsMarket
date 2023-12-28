177c18db411774d001221af1861dfd4f20376cb7
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-28 14:27:30 +0300

    feat: add services interfaces

---
+++ Services/Chat/Chat.Application/Interfaces/Services/IChatService.cs
@@ -0,0 +1,10 @@
+﻿using Chat.Application.DTOs.Chat;
+
+namespace Chat.Application.Interfaces.Services;
+
+public interface IChatService
+{
+    Task<IEnumerable<GetChatDto>> GetChatsAsync(CancellationToken cancellationToken = default);
+    
+    Task<GetChatDto> CreateChatAsync(CreateChatDto createChatDto, CancellationToken cancellationToken = default);
+}
---
+++ Services/Chat/Chat.Application/Interfaces/Services/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using Chat.Application.DTOs.Message;
+
+namespace Chat.Application.Interfaces.Services;
+
+public interface IMessageService
+{
+    Task<IEnumerable<GetMessageDto>> GetMessagesByChatIdAsync(Guid chatId, CancellationToken cancellationToken = default);
+    
+    Task<GetMessageDto> SendMessageAsync(SendMessageDto sendMessageDto, CancellationToken cancellationToken = default);
+}
