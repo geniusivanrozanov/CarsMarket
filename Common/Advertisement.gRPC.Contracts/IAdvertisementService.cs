63e4c8b5ad14ac30dbb22334752346b87797dc71
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-28 14:57:03 +0300

    feat: add advertisement service grpc contracts

---
+++ Common/Advertisement.gRPC.Contracts/IAdvertisementService.cs
@@ -0,0 +1,10 @@
+﻿using Advertisement.gRPC.Contracts.Replies;
+using Advertisement.gRPC.Contracts.Requests;
+using ProtoBuf.Grpc;
+
+namespace Advertisement.gRPC.Contracts;
+
+public interface IAdvertisementService
+{
+    Task<GetAdInfoByIdReply> GetAdInfoByIdAsync(GetAdInfoByIdRequest request, CallContext context = default);
+}
---
+++ Common/Advertisement.gRPC.Contracts/Requests/GetAdInfoByIdRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Serialization;
+
+namespace Advertisement.gRPC.Contracts.Requests;
+
+[DataContract]
+public class GetAdInfoByIdRequest
+{
+    [DataMember(Order = 1)]
+    public Guid AdId { get; set; }
+}
