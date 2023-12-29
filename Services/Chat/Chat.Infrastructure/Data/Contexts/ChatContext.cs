aaed6d577282acaa1bf36a357c3377ac951b8438
Ivan Rozanov <navi.zor9@gmail.com>
2023-12-29 15:58:57 +0300

    feat: add context

---
+++ Services/Chat/Chat.Infrastructure/Data/Contexts/ChatContext.cs
@@ -0,0 +1,31 @@
+﻿using Chat.Domain.Entities;
+using Chat.Infrastructure.Options;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Chat.Infrastructure.Data.Contexts;
+
+public class ChatContext : MongoContextBase
+{
+    private static bool _isCreated;
+    
+    public IMongoCollection<ChatEntity> Chats { get; }
+    public IMongoCollection<MessageEntity> Messages { get; }
+    
+    protected ChatContext(MongoClient client, IOptions<DatabaseOptions> options) : base(client, options)
+    {
+        Chats = Database.GetCollection<ChatEntity>(nameof(Chats));
+        Messages = Database.GetCollection<MessageEntity>(nameof(Messages));
+        
+        if (!_isCreated)
+        {
+            OnConfiguring();
+            _isCreated = true;
+        }
+    }
+    
+    private void OnConfiguring()
+    {
+        
+    }
+}
