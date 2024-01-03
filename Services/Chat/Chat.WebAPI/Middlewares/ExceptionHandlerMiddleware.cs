4f0447460c236a3c025fd5346f4ebcfce17bd842
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-03 14:13:40 +0300

    feat: add exception handler middleware

---
+++ Services/Chat/Chat.WebAPI/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using Chat.Application.Exceptions;
+
+namespace Chat.WebAPI.Middlewares;
+
+public class ExceptionHandlerMiddleware : IMiddleware
+{
+    private readonly Dictionary<Type, HttpStatusCode> _statusCodes = new()
+    {
+        { typeof(NotExistsException), HttpStatusCode.NotFound },
+        { typeof(ForbiddenActionException), HttpStatusCode.Forbidden }
+    };
+
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch (Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            if (_statusCodes.TryGetValue(exception.GetType(), out var statusCode))
+            {
+                context.Response.StatusCode = (int)statusCode;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    exception.Message
+                });
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    Message = "Internal server error"
+                });
+
+                _logger.LogError(exception, "Message: {Message}", exception.Message);
+            }
+        }
+    }
+}
--- Services/Chat/Chat.WebAPI/Program.cs
+++ Services/Chat/Chat.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Chat.WebAPI.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -10,6 +12,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.UseHttpsRedirection();
 
