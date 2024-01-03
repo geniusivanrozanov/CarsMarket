6697dcb3dea2a35be3c8b62277eb67bc230368ed
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-03 14:10:50 +0300

    feat: add current user service

---
+++ Services/Chat/Chat.WebAPI/Services/CurrentUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Chat.Application.Interfaces.Services;
+
+namespace Chat.WebAPI.Services;
+
+public class CurrentUser : ICurrentUser
+{
+    private readonly ClaimsPrincipal _user;
+    
+    public Guid Id => Guid.Parse(_user.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+    public CurrentUser(IHttpContextAccessor httpContextAccessor)
+    {
+        _user = httpContextAccessor.HttpContext!.User;
+    }
+}
