da38bf5dd39cf68996904ad44061e815403a65a0
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-04 19:59:34 +0300

    refactor: remove react client

--- Clients/react-client/src/constants/urls.js
+++
@@ -1,2 +0,0 @@
-export const CHAT_URL = "http://localhost:8003"
-export const IDENTITY_URL = "http://localhost:8000"
--- Clients/react-client/src/pages/Root/Root.jsx
+++
@@ -1,9 +0,0 @@
-const Root = () => {
-    return (
-        <div>
-            <h1>Root page</h1>
-        </div>
-    )
-}
-
-export default Root
