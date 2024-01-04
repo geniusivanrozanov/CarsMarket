d09c815170b5f57b7edb3e8d0f238b40c9e226fb
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-04 15:35:46 +0300

    fix: add contract attributes to advertisement service grpc contract

--- Common/Advertisement.gRPC.Contracts/IAdvertisementService.cs
+++ Common/Advertisement.gRPC.Contracts/IAdvertisementService.cs
@@ -1,10 +1,13 @@
-﻿using Advertisement.gRPC.Contracts.Replies;
+﻿using System.ServiceModel;
+using Advertisement.gRPC.Contracts.Replies;
 using Advertisement.gRPC.Contracts.Requests;
 using ProtoBuf.Grpc;
 
 namespace Advertisement.gRPC.Contracts;
 
+[ServiceContract]
 public interface IAdvertisementService
 {
+    [OperationContract]
     Task<GetAdInfoByIdReply> GetAdInfoByIdAsync(GetAdInfoByIdRequest request, CallContext context = default);
 }
