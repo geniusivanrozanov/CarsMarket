6ba070fa426f117e90554672273db03ec2d28609
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-06 10:04:06 +0300

    feat: add notification service grpc contracts

---
+++ Common/Notification.gRPC.Contracts/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+using Notification.gRPC.Contracts.Replies;
+using Notification.gRPC.Contracts.Requests;
+using ProtoBuf.Grpc;
+
+namespace Notification.gRPC.Contracts;
+
+[ServiceContract]
+public interface INotificationService
+{
+    [OperationContract]
+    Task<SendEmailReply> SendEmailAsync(SendEmailRequest request, CallContext context = default);
+}
