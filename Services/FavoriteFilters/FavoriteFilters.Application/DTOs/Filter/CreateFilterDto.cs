41f22052539edb6a26d7bf6bc8a42f36861d991d
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-06 13:12:14 +0300

    feat: add dtos

---
+++ Services/FavoriteFilters/FavoriteFilters.Application/DTOs/Filter/CreateFilterDto.cs
@@ -0,0 +1,17 @@
+﻿using FavoriteFilters.Domain.Enums;
+
+namespace FavoriteFilters.Application.DTOs.Filter;
+
+public class CreateFilterDto
+{
+    public Guid? BrandId { get; set; }
+    public Guid? ModelId { get; set; }
+    public Guid? GenerationId { get; set; }
+    public int? MinYear { get; set; }
+    public int? MaxYear { get; set; }
+    public int? MinMileage { get; set; }
+    public int? MaxMileage { get; set; }
+    public double? MinPrice { get; set; }
+    public double? MaxPrice { get; set; }
+    public Currency? Currency { get; set; }
+}
