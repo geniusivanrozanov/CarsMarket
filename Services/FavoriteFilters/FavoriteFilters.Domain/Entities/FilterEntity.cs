26edbe1bd5a7130b3fb05d3034a81433dd26b386
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-06 13:06:05 +0300

    feat: add filter entity

---
+++ Services/FavoriteFilters/FavoriteFilters.Domain/Entities/FilterEntity.cs
@@ -0,0 +1,18 @@
+﻿using FavoriteFilters.Domain.Enums;
+
+namespace FavoriteFilters.Domain.Entities;
+
+public class FilterEntity
+{
+    public Guid Id { get; set; }
+    public Guid? BrandId { get; set; }
+    public Guid? ModelId { get; set; }
+    public Guid? GenerationId { get; set; }
+    public int? MinYear { get; set; }
+    public int? MaxYear { get; set; }
+    public int? MinMileage { get; set; }
+    public int? MaxMileage { get; set; }
+    public double? MinPrice { get; set; }
+    public double? MaxPrice { get; set; }
+    public Currency? Currency { get; set; }
+}
