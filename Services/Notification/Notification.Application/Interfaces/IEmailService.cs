6598a1fd311bd579161be26184dd73579d4a1bc1
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-06 10:19:07 +0300

    feat: add email service interface

---
+++ Services/Notification/Notification.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Notification.Application.Interfaces;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string recipientEmailAddress, string subject, string body,
+        CancellationToken cancellationToken = default);
+}
