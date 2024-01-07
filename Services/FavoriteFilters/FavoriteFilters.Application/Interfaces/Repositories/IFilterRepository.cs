c9c60b4a21a36a7eabf32b7bad9c7e2b03a4b33a
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-07 12:21:03 +0300

    feat: add filter repository interface

---
+++ Services/FavoriteFilters/FavoriteFilters.Application/Interfaces/Repositories/IFilterRepository.cs
@@ -0,0 +1,10 @@
+﻿using FavoriteFilters.Domain.Entities;
+
+namespace FavoriteFilters.Application.Interfaces.Repositories;
+
+public interface IFilterRepository : IRepositoryBase<FilterEntity>
+{
+    Task<TProjection?> GetFilterByIdAsync<TProjection>(Guid filterId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TProjection>> GetFiltersAsync<TProjection>(CancellationToken cancellationToken = default);
+    Task<bool> ExistsWithIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
