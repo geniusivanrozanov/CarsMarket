534d0714471ebc541de30faa0095f6dc91a09d26
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-07 15:04:15 +0300

    feat: add infrastructure layer di registration

---
+++ Services/FavoriteFilters/FavoriteFilters.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using FavoriteFilters.Application.Interfaces.Repositories;
+using FavoriteFilters.Infrastructure.Data.Contexts;
+using FavoriteFilters.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FavoriteFilters.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddDbContexts(configuration)
+            .AddRepositories();
+        
+        return services;
+    }
+    
+    private static IServiceCollection AddDbContexts(this IServiceCollection services, IConfiguration configuration)
+    {
+        var filtersConnectionString = configuration.GetConnectionString("FiltersPostgres");
+        ArgumentException.ThrowIfNullOrEmpty(filtersConnectionString);
+
+        services.AddDbContext<FiltersContext>(builder => { builder.UseNpgsql(filtersConnectionString); });
+
+        return services;
+    }
+    
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IRepositoryUnitOfWork, RepositoryUnitOfWork>();
+        services.AddScoped<IFilterRepository, FilterRepository>();
+
+        return services;
+    }
+}
