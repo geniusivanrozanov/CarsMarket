78b1b24659f6c51a48a86b0bec2d0202ea221a4d
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-07 14:45:20 +0300

    feat: add filters db context

---
+++ Services/FavoriteFilters/FavoriteFilters.Infrastructure/Data/Contexts/FiltersContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using FavoriteFilters.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FavoriteFilters.Infrastructure.Data.Contexts;
+
+public class FiltersContext : DbContext
+{
+    public FiltersContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<FilterEntity> Filters { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Services/FavoriteFilters/FavoriteFilters.Infrastructure/Data/Contexts/FiltersContextDesignFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FavoriteFilters.Infrastructure.Data.Contexts;
+
+public class FiltersContextDesignFactory : IDesignTimeDbContextFactory<FiltersContext>
+{
+    public FiltersContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<FiltersContext>();
+        optionsBuilder.UseNpgsql();
+
+        return new FiltersContext(optionsBuilder.Options);
+    }
+}
