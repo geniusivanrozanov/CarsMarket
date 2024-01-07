242d35b47462c5cb62e62b66e84e042918aee455
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-07 13:00:01 +0300

    feat: add filter queries

---
+++ Services/FavoriteFilters/FavoriteFilters.Application/Features/Queries/Filter/GetFilterById/GetFilterByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using FavoriteFilters.Application.DTOs.Filter;
+using FavoriteFilters.Application.Exceptions;
+using FavoriteFilters.Application.Features.Commands.Filter.UpdateFilter;
+using FavoriteFilters.Application.Interfaces.Repositories;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace FavoriteFilters.Application.Features.Queries.Filter.GetFilterById;
+
+public class GetFilterByIdQueryHandler : IRequestHandler<GetFilterByIdQuery, GetFilterDto>
+{
+    private readonly IFilterRepository _filterRepository;
+    private readonly ILogger<GetFilterByIdQueryHandler> _logger;
+
+    public GetFilterByIdQueryHandler(IRepositoryUnitOfWork repositoryUnitOfWork, ILogger<GetFilterByIdQueryHandler> logger)
+    {
+        _logger = logger;
+        _filterRepository = repositoryUnitOfWork.Filters;
+    }
+    
+    public async Task<GetFilterDto> Handle(GetFilterByIdQuery request, CancellationToken cancellationToken)
+    {
+        var dto = await _filterRepository.GetFilterByIdAsync<GetFilterDto>(request.FilterId, cancellationToken);
+
+        if (dto is null)
+        {
+            _logger.LogInformation("Filter with id {Id} not exists", request.FilterId);
+            throw new NotExistsException($"Filter with id '{request.FilterId}' not exists.");
+        }
+
+        return dto;
+    }
+}
---
+++ Services/FavoriteFilters/FavoriteFilters.Application/Features/Queries/Filter/GetFiltersList/GetFiltersListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using FavoriteFilters.Application.DTOs.Filter;
+using FavoriteFilters.Application.Features.Commands.Filter.UpdateFilter;
+using FavoriteFilters.Application.Interfaces.Repositories;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace FavoriteFilters.Application.Features.Queries.Filter.GetFiltersList;
+
+public class GetFiltersListQueryHandler : IRequestHandler<GetFiltersListQuery, IEnumerable<GetFilterDto>>
+{
+    private readonly IFilterRepository _filterRepository;
+
+    public GetFiltersListQueryHandler(IRepositoryUnitOfWork repositoryUnitOfWork)
+    {
+        _filterRepository = repositoryUnitOfWork.Filters;
+    }
+    
+    public async Task<IEnumerable<GetFilterDto>> Handle(GetFiltersListQuery request, CancellationToken cancellationToken)
+    {
+        var dto = await _filterRepository.GetFiltersAsync<GetFilterDto>(cancellationToken);
+
+        return dto;
+    }
+}
