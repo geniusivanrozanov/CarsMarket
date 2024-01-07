abca6001f8e5aa6028e9f1a850b4573172d3c4bf
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-07 14:38:35 +0300

    feat: add application layer di registration

---
+++ Services/FavoriteFilters/FavoriteFilters.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FavoriteFilters.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediator();
+        
+        return services;
+    }
+    
+    private static IServiceCollection AddMediator(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
