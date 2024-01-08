1dbfa61d5b3bf484ebabdf66a45ae52ef33b30d8
Ivan Rozanov <navi.zor9@gmail.com>
2024-01-08 16:07:03 +0300

    feat: add get ads by query parameters method to rpc contracts

---
+++ Common/Advertisement.gRPC.Contracts/Replies/GetAdsByQueryParametersReply.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+using Advertisement.gRPC.Contracts.Models;
+
+namespace Advertisement.gRPC.Contracts.Replies;
+
+[DataContract]
+public class GetAdsByQueryParametersReply
+{
+    [DataMember(Order = 1)]
+    public IEnumerable<AdDataContract> Ads { get; set; }
+}
